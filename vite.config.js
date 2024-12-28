f32f6dbcc147c6cba39d850733b46bba84bc1636
DolphAI <peek.nahar@gmail.com>
2024-12-29 01:06:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,6 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: true
   },
   optimizeDeps: {
     include: ['framer-motion']
